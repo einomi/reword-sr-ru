b3300bfe9c3c8e2517dfda93b862428eaee152c3
Andrey Nikonov <a.einomi@gmail.com>
2023-12-29 00:27:40 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,151 @@
+// There is the following content in the input.txt file:
+//
+// ```
+// первый второй третий четвёртый пятый
+// шестой седьмой восьмой девятый десятый
+//
+// први други трећи четврти пети
+// шести седми осми девети десети
+// ```
+//
+// Now I want to parse it with node.js, read both paragraphs, replace all space characters with empty lines and make the first paragraph a first column in a csv file called output.csv, and the second one as a second column. Use  as a separator. Also, for the second paragraph which is in serpski language I want to use latin notation.
+//
+// I also want to preserve the content of output.csv in order to save the previously added content.
+
+/** @param {string} input */
+function serbianCyrillicToLatin(input) {
+  const map = {
+    а: "a",
+    б: "b",
+    в: "v",
+    г: "g",
+    д: "d",
+    ђ: "đ",
+    е: "e",
+    ж: "ž",
+    з: "z",
+    и: "i",
+    ј: "j",
+    к: "k",
+    л: "l",
+    љ: "lj",
+    м: "m",
+    н: "n",
+    њ: "nj",
+    о: "o",
+    п: "p",
+    р: "r",
+    с: "s",
+    т: "t",
+    ћ: "ć",
+    у: "u",
+    ф: "f",
+    х: "h",
+    ц: "c",
+    ч: "č",
+    џ: "dž",
+    ш: "š",
+    А: "A",
+    Б: "B",
+    В: "V",
+    Г: "G",
+    Д: "D",
+    Ђ: "Đ",
+    Е: "E",
+    Ж: "Ž",
+    З: "Z",
+    И: "I",
+    Ј: "J",
+    К: "K",
+    Л: "L",
+    Љ: "Lj",
+    М: "M",
+    Н: "N",
+    Њ: "Nj",
+    О: "O",
+    П: "P",
+    Р: "R",
+    С: "S",
+    Т: "T",
+    Ћ: "Ć",
+    У: "U",
+    Ф: "F",
+    Х: "H",
+    Ц: "C",
+    Ч: "Č",
+    Џ: "Dž",
+    Ш: "Š",
+  };
+  return input.replace(/./g, (char) => map[char] || char);
+}
+
+const INPUT_SEPARATOR = "\n";
+
+function createCsvRecordArray(input) {
+  return (
+    input
+      .split(INPUT_SEPARATOR)
+      // remove null and tab characters
+      .map((item) => item.replace(/\0|\t/g, ""))
+      // replace new line characters with space
+      .map((item) => item.replace(/\r?\n|\r/g, " "))
+      .map((item) => item.trim())
+      .filter((item) => item)
+  );
+}
+
+/** @param {string} cyrillicWord */
+function putEmphasisForSerbianWord(cyrillicWord) {
+
+}
+
+const fs = require("fs");
+const { createObjectCsvWriter } = require("csv-writer");
+
+const INPUT_FILE_NAME = "numbers-other.txt";
+const OUTPUT_FILE_NAME = `${INPUT_FILE_NAME.split(".")[0]}.csv`;
+
+// Read the contents of the input file
+fs.readFile(INPUT_FILE_NAME, "utf8", (err, data) => {
+  if (err) throw err;
+
+  // Split the data into two paragraphs
+  const [paragraph1, paragraph2] = data.split("\n\n");
+
+  // Replace space characters with empty lines
+  const formattedColumn1 = createCsvRecordArray(serbianCyrillicToLatin(paragraph2));
+  const formattedColumn2 = createCsvRecordArray(paragraph2);
+  const formattedColumn3 = createCsvRecordArray(paragraph1);
+
+  console.log("formattedColumn1", formattedColumn1);
+  console.log("formattedColumn2", formattedColumn2);
+  console.log("formattedColumn3", formattedColumn3);
+
+  if (
+    formattedColumn1.length !== formattedColumn2.length ||
+    formattedColumn2.length !== formattedColumn3.length
+  ) {
+    throw new Error("The number of records in the columns is not equal!");
+  }
+
+  // Create a CSV writer
+  const csvWriter = createObjectCsvWriter({
+    path: OUTPUT_FILE_NAME,
+    header: ["column1", "column2", "column3"],
+    append: false,
+    fieldDelimiter: ";",
+    recordDelimiter: "\n",
+  });
+
+  // transform columns into records
+  const records = formattedColumn1.map((item, index) => ({
+    column1: formattedColumn1[index],
+    column2: formattedColumn2[index],
+    column3: formattedColumn3[index],
+  }));
+
+  // Write the formatted data to the CSV file
+  csvWriter.writeRecords(records).then(() => {
+    console.log(`...Done`);
+  });
+});
---
+++ post-process.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+
+// А́а́, Е́е́, И́и́, О́о́, У́у́, Ы́ы́, Э́э́, Ю́ю́, Я́я́
+function convertToAccented(word) {
+  const accentsMap = {
+    'А': 'а́',
+    'Б': 'б',
+    'В': 'в',
+    'Г': 'г',
+    'Д': 'д',
+    'Е': 'е́',
+    'Ё': 'ё',
+    'Ж': 'ж',
+    'З': 'з',
+    'И': 'и́',
+    'Й': 'й',
+    'К': 'к',
+    'Л': 'л',
+    'М': 'м',
+    'Н': 'н',
+    'О': 'о́',
+    'П': 'п',
+    'Р': 'р',
+    'С': 'с',
+    'Т': 'т',
+    'У': 'у́',
+    'Ф': 'ф',
+    'Х': 'х',
+    'Ц': 'ц',
+    'Ч': 'ч',
+    'Ш': 'ш',
+    'Щ': 'щ',
+    'Ъ': 'ъ',
+    'Ы': 'ы́',
+    'Ь': 'ь',
+    'Э': 'э́',
+    'Ю': 'ю́',
+    'Я': 'я́'
+  };
+
+  const convertedWord = word.split('').map(char => {
+    if (char in accentsMap) {
+      return accentsMap[char];
+    }
+    return char;
+  }).join('');
+
+  return convertedWord;
+}
+
+const inputFile = 'numbers.csv';
+const outputFile = `${inputFile.split('.')[0]}.processed.csv`
+
+fs.readFile(inputFile, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Разделение данных на строки
+  const lines = data.split('\n');
+
+  // Обработка и преобразование данных
+  const convertedLines = lines.map(line => {
+    const columns = line.split(';');
+    if (columns.length >= 3) {
+      columns[1] = convertToAccented(columns[1]);
+    }
+    return columns.join(';');
+  });
+
+  // Запись результата в новый файл
+  fs.writeFile(outputFile, convertedLines.join('\n'), 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Обработанные данные сохранены в', outputFile);
+  });
+});
